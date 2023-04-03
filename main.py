6abba7b18b967d0b82ebb14507b14e755886496b
Ian <100323825+IanM21@users.noreply.github.com>
2023-04-03 14:30:47 -0400

    fully rewritten
    had to do a full rewrite for this due to pytube becoming broken for the time being.

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-from pytube import YouTube
+import os
+import youtube_dl
+
 while True:
-    def YT_download():
-        url = input("Enter the url of the video: ")
-        yt = YouTube(url)
-        yt.streams.filter(res='720p').first().download('./Youtube')
-        print("Download Successful for {}".format(url))
-        print("\n")  
-    YT_download()
+    link = input("Enter YouTube video link (or 'q' to quit): ")
+    if link == 'q':
+        print("Thanks for using my script. Goodbye!")
+        break
+    
+    ydl_opts = {
+        'outtmpl': 'yt-downloads/%(title)s.%(ext)s',
+        'format': 'best'
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        try:
+            info_dict = ydl.extract_info(link, download=True)
+            video_title = info_dict.get('title', None)
+            print(f"Successfully downloaded {video_title}")
+        except youtube_dl.utils.DownloadError as e:
+            print(f"Unable to download video: {str(e)}")
